e75fa057badeaead5d40bb1b6f88e5ce35d75cab
Nhan Truong <nhantruong1298@gmail.com>
2024-10-16 16:59:52 +0700

    Initial Commit

---
+++ GithubSearch/GithubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearchApp.swift
+//  GithubSearch
+//
+//  Created by Nhân Trương on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
